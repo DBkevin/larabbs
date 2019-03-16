8cde9f338ae092da288c2ec6125755118c8a57d0
Leedocker <416606903@outlook.com>
2019-03-16 09:18:13 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,16 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    public function category()
+    { 
+        //1对1关联,相对关联
+        //因为一个文章只有一个分类ID
+        return $this->belongsTo(Category::class);
+    }
+    public function user(){
+        //1对1关联,相对关联用beLogsTo
+        //因为一个文章只有一个用户
+        return $this->belongsTo(User::class);
+    }
+
 }
