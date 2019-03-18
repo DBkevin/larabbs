33cd53df35c6bd03d61d7cf72da3571413b7b563
Leedocker <416606903@outlook.com>
2019-03-18 03:22:23 +0000

    分类话题列表

--- app/helper.php
+++ app/helper.php
@@ -1,5 +1,18 @@
 <?php
-function route_class(){
+function route_class()
+{
     //获取当前路由名称,吧'.'替换为-后返回
-    return str_replace('.','-',Route::currentRouteAction());
+    return str_replace('.', '-', Route::currentRouteAction());
 }
+/**
+ * 判定是否给active
+ *
+ * @param [int] $category_id
+ * @return void
+ */
+function category_nav_active($category_id)
+{
+    return  active_class(if_route('categories.show') && if_route_param('category', $category_id));
+    //return  active_class(if_route('categories.show') && if_route_param('category', $category_id),"aaaa",'bbb');
+}
+
