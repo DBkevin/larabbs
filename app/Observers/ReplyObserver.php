d9776f656db83fb6b1234259e9553bf07044335d
Leedocker <416606903@outlook.com>
2019-03-19 10:26:21 +0000

    回复xss

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -18,4 +18,21 @@ class ReplyObserver
     {
         //
     }
+    public function saving(Reply $reply)
+    {
+        //过滤XSS
+        //$reply->content=clean($reply->content,'user_topic_body');
+        //判断过滤后是否为空
+        $content = clean($reply->content, 'user_topic_body');
+        if (empty($content)) {
+            $content = '空';
+        } else {
+            $reply->content = $content;
+            //不严谨的做法
+            // $reply->topic->increment('reply_count',1);
+            $reply->topic->reply_count = $reply->topic->replies->count();
+            $reply->topic->save();
+        }
+    }
 }
+
